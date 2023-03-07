b32b93857fd55da97c651251411ba5731ec7f86f
Seth H <csharpseth@gmail.com>
2023-03-07 15:28:19 -0600

    performance - fixes

--- src/views/components/NavbarComponent.tsx
+++ src/views/components/NavbarComponent.tsx
@@ -1,55 +1,50 @@
 /** @format */
 
-import { useContext, useEffect, useRef, useState } from 'react'
-import { DisplayContext } from '../../contexts/DisplayContext'
-import { NavigationContext } from '../../contexts/NavigationContext'
-import '../../styles/Navbar.scss'
-import HamburgerButtonComponent from './HamburgerButtonComponent'
-import { Link } from './UtilityComponents'
+import { useContext, useEffect, useRef, useState } from 'react';
+import { DisplayContext } from '../../contexts/DisplayContext';
+import { NavigationContext } from '../../contexts/NavigationContext';
+import '../../styles/Navbar.scss';
+import { Link } from './UtilityComponents';
 
 function NavbarItem(props: any) {
-	const { link, to, value, onClick } = props
-	const { currentPath } = useContext(NavigationContext)
+	const { link, to, value, onClick } = props;
+	const { currentPath } = useContext(NavigationContext);
 
 	return (
 		<Link
 			to={to}
 			link={link}
 			onClick={() => {
-				if (onClick) onClick()
+				if (onClick) onClick();
 			}}
 			className={currentPath === link ? 'active' : ''}
 		>
 			<span>{value}</span>
 		</Link>
-	)
+	);
 }
 
 export default function NavbarComponent() {
-	const { scrollPercent, scrollHeight } = useContext(DisplayContext)
-	const [open, setOpen] = useState(false)
-	const [visible, setVisible] = useState<boolean>(true)
+	const { scrollPercent, scrollHeight } = useContext(DisplayContext);
+	const [open, setOpen] = useState(false);
+	const [visible, setVisible] = useState<boolean>(true);
 
-	const headerRef = useRef<HTMLDivElement>(null)
+	const headerRef = useRef<HTMLDivElement>(null);
 
 	function Close() {
-		setOpen(false)
+		setOpen(false);
 	}
 
 	useEffect(() => {
-		if (!headerRef.current) return
+		if (!headerRef.current) return;
 
-		const height = headerRef.current.offsetHeight
-		const portion = height / scrollHeight
-		setVisible(scrollPercent <= portion)
-	}, [scrollPercent, headerRef])
+		const height = headerRef.current.offsetHeight;
+		const portion = height / scrollHeight;
+		setVisible(scrollPercent <= portion);
+	}, [scrollPercent, headerRef, scrollHeight]);
 
 	return (
 		<>
-			<HamburgerButtonComponent
-				value={open}
-				onChange={(open: boolean) => setOpen(open)}
-			/>
 			<div
 				className={`navbar-header ${visible ? 'visible' : ''}`}
 				ref={headerRef}
@@ -65,5 +60,5 @@ export default function NavbarComponent() {
 				<NavbarItem link="/articles" value="Articles" onClick={Close} />
 			</nav>
 		</>
-	)
+	);
 }
--- src/views/pages/HomePage.tsx
+++ src/views/pages/HomePage.tsx
@@ -5,11 +5,14 @@ import { QuarterPageSpacerComponent } from '../components/PageSpacerComponent';
 import ContainerComponent from '../components/ContainerComponent';
 
 import '../../styles/HomePage.scss';
-import { lazy } from 'react';
+//import { lazy } from 'react';
+import AboutSection from '../sections/AboutSection';
+import ProjectSection from '../sections/ProjectSection';
+import ContactSection from '../sections/ContactSection';
 
-const AboutSection = lazy(() => import('../sections/AboutSection'));
-const ProjectSection = lazy(() => import('../sections/ProjectSection'));
-const ContactSection = lazy(() => import('../sections/ContactSection'));
+// const AboutSection = lazy(() => import('../sections/AboutSection'));
+// const ProjectSection = lazy(() => import('../sections/ProjectSection'));
+// const ContactSection = lazy(() => import('../sections/ContactSection'));
 
 export default function HomePage(props: any) {
 	return (
